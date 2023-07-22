675dbe8fefa08be94fea4463050f9a30ce03f8c7
marinesnow34 <m4rinesnow34@gmail.com>
2023-07-22 13:25:00 +0900

    Init: 초기 세팅 파일

---
+++ src/main/java/com/back/splitmeet/SplitmeetApplication.java
@@ -0,0 +1,13 @@
+package com.back.splitmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitmeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/splitmeet/SplitmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.splitmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
