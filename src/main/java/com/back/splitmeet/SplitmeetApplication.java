78af39033c3797208387b3cf68df17f49d4057a8
1223v <78861124+1223v@users.noreply.github.com>
2023-08-01 13:45:15 +0900

    Fix: git 오류 수정
    git pull로 인한 문제 수정

--- src/main/java/com/back/splitmeet/SplitmeetApplication.java
+++ src/main/java/com/back/splitmeet/SplitmeetApplication.java
@@ -9,8 +9,4 @@ public class SplitmeetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SplitmeetApplication.class, args);
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> 91b63350ea0e19ae81ad1c606e06f60663b157a1
 }
