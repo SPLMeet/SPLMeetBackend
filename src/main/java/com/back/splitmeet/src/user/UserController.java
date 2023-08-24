cdbf8ef388014ff709fb173cbf7ce1b8996d8f67
marinesnow34 <m4rinesnow34@gmail.com>
2023-08-24 15:55:12 +0900

    Test: 템플릿 코드 작성

--- src/main/java/com/back/splitmeet/src/user/UserController.java
+++ src/main/java/com/back/splitmeet/src/user/UserController.java
@@ -1,10 +1,29 @@
 package com.back.splitmeet.src.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.back.splitmeet.src.user.dto.PostUserTestReq;
+import com.back.splitmeet.src.user.dto.PostUserTestRes;
+import com.back.splitmeet.util.BaseResponse;
+import com.back.splitmeet.util.BaseResponseStatus;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+	@Autowired
+	private UserService userService;
 
+	@PostMapping("/test")
+	public BaseResponse<PostUserTestRes> test(@RequestBody PostUserTestReq req) {
+		PostUserTestRes postUserTestRes = userService.test(req);
+		if (postUserTestRes.getUserIdx() == 1) {
+			return new BaseResponse<>(BaseResponseStatus.EMPTY_JWT);
+		}
+
+		return new BaseResponse<>(postUserTestRes);
+	}
 }
---
+++ src/main/java/com/back/splitmeet/src/user/dto/PostUserTestReq.java
@@ -0,0 +1,8 @@
+package com.back.splitmeet.src.user.dto;
+
+import lombok.Data;
+
+@Data
+public class PostUserTestReq {
+	private String userName;
+}
