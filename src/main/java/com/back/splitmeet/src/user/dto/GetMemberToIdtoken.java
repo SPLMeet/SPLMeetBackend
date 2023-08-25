08b125148affefc993b8cc5daa61a1dd7baac894
scott4935 <scott4935@gmail.com>
2023-08-25 18:41:06 +0900

    Fix: NoArgsConstructor 추가

--- src/main/java/com/back/splitmeet/src/user/dto/GetMemberToIdtoken.java
+++ src/main/java/com/back/splitmeet/src/user/dto/GetMemberToIdtoken.java
@@ -2,8 +2,10 @@ package com.back.splitmeet.src.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
@@ -12,7 +14,4 @@ public class GetMemberToIdtoken {
 	private String email;
 	private String picture;
 
-	// 삭제 금지, 미사용으로 뜨지만 필요함 json 관리에 필요함
-	public GetMemberToIdtoken() {
-	}
 }
