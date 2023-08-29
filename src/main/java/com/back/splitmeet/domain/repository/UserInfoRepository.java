bced96254c04e99d64a9f4ee3e531558b61c20f4
greensnapback0229 <smdmim@gmail.com>
2023-08-29 17:33:21 +0900

    Delete UserInfoRepository.java

--- src/main/java/com/back/splitmeet/domain/repository/UserInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.back.splitmeet.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.back.splitmeet.domain.UserInfo;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
-}
