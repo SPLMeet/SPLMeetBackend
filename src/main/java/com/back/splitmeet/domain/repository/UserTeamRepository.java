26e6676a3f41fd432d1471325d50edfd775eda79
scott4935 <scott4935@gmail.com>
2023-09-15 23:49:30 +0900

    Feat: UserTeamRepository 추가

---
+++ src/main/java/com/back/splitmeet/domain/repository/UserTeamRepository.java
@@ -0,0 +1,9 @@
+package com.back.splitmeet.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.back.splitmeet.domain.UserTeam;
+
+public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
+	UserTeam findOneByTeamId(Long teamId);
+}
