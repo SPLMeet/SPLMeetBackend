ab20b13435423198924efe76f45f2cb9a57603f9
scott4935 <scott4935@gmail.com>
2023-09-18 14:19:03 +0900

    Fix: 팀 생성을 위한 dto 추가

---
+++ src/main/java/com/back/splitmeet/src/team/dto/PostCreateTeamRes.java
@@ -0,0 +1,12 @@
+package com.back.splitmeet.src.team.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PostCreateTeamRes {
+	private Long team_idx;
+}
