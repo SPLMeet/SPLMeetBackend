2eed17c0e4af7334d2161c73c58f2d2351a5273a
scott4935 <scott4935@gmail.com>
2023-09-18 20:20:12 +0900

    Feat: column default 추가 및 불필요 인자 제거

--- src/main/java/com/back/splitmeet/domain/UserTeam.java
+++ src/main/java/com/back/splitmeet/domain/UserTeam.java
@@ -32,7 +32,7 @@ public class UserTeam {
 	@Column(nullable = false, columnDefinition = "boolean default false")
 	private Boolean teamSettleStatus; // 팀 정산 여부
 
-	@Column(nullable = false)
+	@Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
 	private LocalDate startDate; // 팀 시작 날짜
 
 	@Column(nullable = false)
@@ -40,14 +40,13 @@ public class UserTeam {
 
 	private Long teamTotalCost; // 팀 총 비용
 
-	@Column(length = 20)
+	@Column(length = 20, columnDefinition = "VARCHAR(20) default 'NONE'")
 	private String leaderKakaoHash; // 팀장 카카오 해시태그
 
 	@Builder
-	public UserTeam(Long teamLeader, String teamName, String leaderKakaoHash) {
+	public UserTeam(Long teamLeader, String teamName) {
 		this.teamLeader = teamLeader;
 		this.teamName = teamName;
-		this.leaderKakaoHash = leaderKakaoHash;
 	}
 
 }
