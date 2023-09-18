a8aafd378d3fc19248cff52f9fdf327c43f194de
scott4935 <scott4935@gmail.com>
2023-09-18 20:19:31 +0900

    Feat: enum 반영

--- src/main/java/com/back/splitmeet/src/user/UserService.java
+++ src/main/java/com/back/splitmeet/src/user/UserService.java
@@ -3,6 +3,7 @@ package com.back.splitmeet.src.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.back.splitmeet.domain.RoleStatus;
 import com.back.splitmeet.domain.UserInfo;
 import com.back.splitmeet.domain.repository.UserInfoRepository;
 import com.back.splitmeet.jwt.JwtTokenProvider;
@@ -43,7 +44,7 @@ public class UserService {
 
 			userInfo = userInfoRepository.findOneByUserEmailAndUserName(idToken.getEmail(),
 				idToken.getNickname());
-			userInfo.setRole(0);
+			userInfo.setRole(RoleStatus.NONE);
 
 			setUserToken(idToken, kakaoLoginRes, userInfo);
 		} else if (action.equals("signin") && userInfo != null) {
