408a0f1c7d6430dbd88004b302c0a392982d3bf7
1223v <1223v@naver.com>
2023-10-08 18:26:59 +0900

    Fix: cascade = CascadeType.ALL 영속화 설정

--- src/main/java/com/back/splitmeet/domain/UserInfo.java
+++ src/main/java/com/back/splitmeet/domain/UserInfo.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -68,7 +69,7 @@ public class UserInfo {
 	@JoinColumn(name = "teamId")
 	private UserTeam userTeam; // 유저 팀
 
-	@OneToMany(mappedBy = "userinfo")
+	@OneToMany(mappedBy = "userinfo", cascade = CascadeType.ALL)
 	private List<PayList> orders = new ArrayList<>(); // 유저 결제 내역
 
 	@Builder(builderMethodName = "userBuilder", buildMethodName = "userBuild")
